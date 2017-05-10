f8f9c225baabf21f7be1b02cf4d755d5bece313a
MONKEY\hdo <huyducdo@googlemail.com>
2017-05-10 15:28:18 +0200

    Objektbewegung mit Navmesh hinzugefügt

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,26 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    NavMeshAgent agent;
+
+    Vector3 viewPoint;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+
+        viewPoint = Camera.main.transform.position + Camera.main.transform.forward * 1;
+        agent.destination = viewPoint;
+    }
+}
