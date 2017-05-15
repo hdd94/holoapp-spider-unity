7fee7c88f2e98dce6916ef61ccf893cb626f1005
MONKEY\hdo <huyducdo@googlemail.com>
2017-05-15 17:27:22 +0200

    Pathfinding funktioniert, Kugeln bewegen sich richtig, NavMesh eingestellt

---
+++ Assets/AddAgent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AddAgent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.gameObject.AddComponent<MoveTo>();
+        Destroy(this);
+    }
+}
--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -10,17 +10,22 @@ public class MoveTo : MonoBehaviour
 
     Vector3 viewPoint;
 
-    void Start()
+    void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        if (agent == null)
+        {
+            agent = this.gameObject.AddComponent<NavMeshAgent>();
+        }
     }
 
     private void Update()
     {
-        //agent.destination = goal.position;
-
-        viewPoint = Camera.main.transform.position + Camera.main.transform.forward * 1;
-        //viewPoint = goal.position + goal.forward * 1;
-        agent.destination = viewPoint;
+        
+            //agent.destination = goal.position;
+            viewPoint = Camera.main.transform.position + Camera.main.transform.forward * 1;
+            //viewPoint = goal.position + goal.forward * 1;
+            agent.destination = viewPoint;
+            //agent.SetDestination(GameObject.Find("Camera").transform.position);
     }
 }
