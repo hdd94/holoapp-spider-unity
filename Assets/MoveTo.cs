fa18d13e62411da48b8650ff35f64569ace7153d
MONKEY\hdo <huyducdo@googlemail.com>
2017-05-16 17:42:40 +0200

    Anfang die Spinne einzubinden

--- Assets/MoveTo.cs
+++ Assets/MoveTo.cs
@@ -16,14 +16,14 @@ public class MoveTo : MonoBehaviour
         if (agent == null)
         {
             agent = this.gameObject.AddComponent<NavMeshAgent>();
+            agent.speed = 1;
         }
     }
-
+  
     private void Update()
     {
-        
             //agent.destination = goal.position;
-            viewPoint = Camera.main.transform.position + Camera.main.transform.forward * 1;
+            viewPoint = Camera.main.transform.position;
             //viewPoint = goal.position + goal.forward * 1;
             agent.destination = viewPoint;
             //agent.SetDestination(GameObject.Find("Camera").transform.position);
